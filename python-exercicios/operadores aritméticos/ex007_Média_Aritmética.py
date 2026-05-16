abef8bdc7b1239db874efc48e9e1e3306fc9f63a
Deyvid Gabriel <deyvidgabrielnascimento@gmail.com>
2026-05-16 18:06:13 -0300

    Exercício 007

---
+++ python-exercicios/operadores aritméticos/ex007_Média_Aritmética.py
@@ -0,0 +1,6 @@
+primeira_nota = float(input("Primeira nota do aluno: "))
+segunda_nota = float(input("Segunda nota do aluno: "))
+
+media = (primeira_nota + segunda_nota) / 2
+
+print(f"A media entre {primeira_nota:.1f} e {segunda_nota:.1f} é igual a {media:.1f}")
