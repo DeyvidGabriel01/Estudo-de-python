e88c921f515b82a6a22e06d2014efd7027c5b421
Deyvid Gabriel <deyvidgabrielnascimento@gmail.com>
2026-05-20 19:14:20 -0300

    Exercício 010

--- python-exercicios/operadores aritméticos/ex010_Conversor_de_Moedas.py
+++ python-exercicios/operadores aritméticos/ex010_Conversor_de_Moedas.py
@@ -1,3 +1,3 @@
 real = float(input("Quanto dinheiro você tem na carteira? R$"))
 dolar = real * 0.20
-print(F"Com R${real} você pode comprer US${dolar:.2f}")
+print(F"Com R${real:.2f} você pode comprer US${dolar:.2f}")
