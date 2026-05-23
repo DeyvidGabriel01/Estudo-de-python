384124614fc24dd527f06d038bddc8873ffc7708
Deyvid Gabriel <deyvidgabrielnascimento@gmail.com>
2026-05-23 19:59:09 -0300

    Exercício 011

---
+++ python-exercicios/operadores aritméticos/ex011_Pintando_Parede.py
@@ -0,0 +1,7 @@
+Largura = float(input("Largura da parede: "))
+Altura = float(input("Altura da parede: "))
+area = Altura * Largura
+tinta  = area / 2
+print(f"Sua parede tem a dimensão de {Largura}X{Altura} e sua área é de {area}m².")
+print(f"Para pintar essa parede, você precisará de {tinta}L de tinta.")
+
