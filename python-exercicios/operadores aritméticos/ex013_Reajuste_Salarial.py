d6b024a9c419359d601a51315c07745af1ea7733
Deyvid Gabriel <deyvidgabrielnascimento@gmail.com>
2026-05-28 11:04:37 -0300

    Exercício 013

---
+++ python-exercicios/operadores aritméticos/ex013_Reajuste_Salarial.py
@@ -0,0 +1,3 @@
+salario = float(input("Qual é o salário do Funcionario? R$"))
+novo_salario = salario + (salario * (15/100))
+print(f"Um funcionario qua ganhava R${salario:.2f}, com 15% de aumento, passa a receber R${novo_salario:.2f}")
