484e6ac425581d92e07c7ae9f8096eb6e33e75a8
Deyvid Gabriel <deyvidgabrielnascimento@gmail.com>
2026-05-28 10:55:18 -0300

    Exercício 012

---
+++ python-exercicios/operadores aritméticos/ex012_Calculando_Descontos.py
@@ -0,0 +1,3 @@
+protudo = float(input("Qual é o preço do produto? R$"))
+desconto = protudo - (protudo * (5/100))
+print(f"O protudo que custava R${protudo:.2f}, na promoção com desconto de 5% vai custa R${desconto:.2f}")
