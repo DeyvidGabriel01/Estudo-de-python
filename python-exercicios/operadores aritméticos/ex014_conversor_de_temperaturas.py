515e501a8452f5a30e58734402381f77d4ec78ca
Deyvid Gabriel <deyvidgabrielnascimento@gmail.com>
2026-07-01 19:18:20 -0300

    Exercício 014

---
+++ python-exercicios/operadores aritméticos/ex014_conversor_de_temperaturas.py
@@ -0,0 +1,3 @@
+graus_celsius = float(input("Informe a temperatura em °C: "))
+graus_fahranheit = (graus_celsius * 1.8) + 32
+print(f"A temperatura de {graus_celsius}°C corresponde a {graus_fahranheit}°F!")
