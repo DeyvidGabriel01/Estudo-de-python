a2e9c441549defdf90785f8b2ce242ac1fb273ec
Deyvid Gabriel <deyvidgabrielnascimento@gmail.com>
2026-07-05 21:15:47 -0300

    Exercício 019

--- python-exercicios/Utilizando Módulos/ex019_Sorteando_um_item_na_lista.py
+++ python-exercicios/Utilizando Módulos/ex019_Sorteando_um_item_na_lista.py
@@ -1,8 +1,8 @@
-import random
+from random import choice
 primeiro_aluno = str(input("Primeiro aluno: "))
 segundo_aluno = str(input("Segundo aluno: "))
 terceiro_aluno = str(input("Terceiro aluno: "))
 quanto_aluno = str(input("Quarto aluno: "))
 lista = [primeiro_aluno, segundo_aluno, terceiro_aluno, quanto_aluno]
-aluno_escolido = random.choice(lista)
+aluno_escolido = choice(lista)
 print(f"O aluno escolhido foi {aluno_escolido}")
