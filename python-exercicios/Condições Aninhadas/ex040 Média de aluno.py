c40483953a5b965d0c0b4eec03d73dc40e75364a
Deyvid Gabriel <deyvidgabrielnascimento@gmail.com>
2026-09-10 14:53:58 -0300

    Exercício 040

---
+++ python-exercicios/Condições Aninhadas/ex040 Média de aluno.py
@@ -0,0 +1,15 @@
+print(f"{' MÉDIA DOS ALUNOS ':=^42}")
+primeira_nota = float(input("\033[36mDigite a primeira nota:\033[m "))
+segunda_nota = float(input("\033[36mDigite a segunda nota:\033[m "))
+terceira_nota = float(input("\033[36mDigite a terceira nota:\033[m "))
+quarta_nota = float(input("\033[36mDigite a quarta nota:\033[m "))
+print("="*42)
+media_final = (primeira_nota + segunda_nota + terceira_nota + quarta_nota) / 4
+print(F"Sua média final é {media_final:.1F}")
+if media_final < 5.0:
+    print("\033[31mO aluno está REPROVADO.\033[m")
+elif media_final >= 5.0 and media_final < 7.0:
+    print("\033[33mO aluno está de RECUPERAÇÃO.\033[m")
+else:
+    print("\033[32mO aluno está APROVADO.\033[m")
+print("="*42)
