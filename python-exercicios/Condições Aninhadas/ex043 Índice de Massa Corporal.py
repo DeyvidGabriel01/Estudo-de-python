2a3e1245b6b4d3852c2f044bf6fb5951ea353fad
Deyvid Gabriel <deyvidgabrielnascimento@gmail.com>
2026-09-17 10:09:35 -0300

    Exercício 043

--- python-exercicios/Condições Aninhadas/ex043 Índice de Massa Corporal.py
+++ python-exercicios/Condições Aninhadas/ex043 Índice de Massa Corporal.py
@@ -5,6 +5,8 @@ print("="*38)
 imc = peso / (altura*altura)
 print(f"O seu IMC é de {imc:.1f}")
 print("="*38)
+
+# metodo 1
 if imc <= 18.5:
     print("Você ABAIXO DO PESO normal")
 elif imc <= 25:
@@ -15,4 +17,17 @@ elif imc <= 40:
     print("Você está em OBESIDADE!")
 else:
     print("Você está em OBESIDADE MÓRBIDA, cuidade!")
+print("="*38)
+
+# medoto 2
+if imc < 18:
+    print("Você está ABAIXO DO PESO normal")
+elif 18 <= imc < 25:
+    print("BARABÉNS, você está na faixa de PESO NORMAL")
+elif 25 <= imc < 30: 
+    print("Você está em SOBREPESO")
+elif 30 <= imc < 40:
+    print("Você está em OBESIDADE, cuidado!")
+elif imc >= 40:
+    print("Você está em OBESIDADE MÓRBIDA, cuidado!")
 print("="*38)
